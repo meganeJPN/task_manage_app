2380b5d5b208ea543fa09f1a123083febbac9cd2
meganeJPN <ooosamuuu1985@gmail.com>
2020-08-28 11:31:59 +0900

    タスクの登録機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,28 @@ class TasksController < ApplicationController
   end
 
   def show
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスクを登録しました！"
+    else
+      render :new
+    end
   end
   
   private
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :content, :status, :priority, :deadline)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, only:[:index,:new, :create, :edit, :show, :destroy]
+  # resources :tasks
 end
