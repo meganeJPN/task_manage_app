9f54e643d7d9177c89ff06bafe9a198d4c37058f
meganeJPN <ooosamuuu1985@gmail.com>
2020-08-28 11:58:47 +0900

    タスクの編集機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit]
   def index
     @tasks = Task.all
   end
@@ -18,6 +18,10 @@ class TasksController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+
   end
   
   private
