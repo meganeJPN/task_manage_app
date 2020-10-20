11745e94f20df4966ef8c3e8be3d440021fd5df9
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-20 15:30:55 +0900

    tasksテーブルのpriorityにNotNull制約を追加 Taskモデルのpriorityに必須入力のvalidationを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255}
   validates :content, presence: true
   validates :status, presence: true
+  validates :priority, presence: true
 
   enum status: {wating: 0, working: 1, completed: 2}
   enum priority: {low: 0, middle: 1, high: 2}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_060236) do
+ActiveRecord::Schema.define(version: 2020_10_20_062920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_060236) do
     t.string "name", null: false
     t.text "content", null: false
     t.integer "status", default: 0, null: false
-    t.integer "priority"
+    t.integer "priority", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deadline"
