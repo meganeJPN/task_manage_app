f93fcead3a4f875fd569a486f93cae4e7e5c49a2
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-22 17:01:14 +0900

    database_cleanerのgem追加 タスク名のあいまい検索とステータスの完全一致検索のsysytem specを作成 タスク名のあいまい検索とステータスの完全一致検索のmodel specを作成

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'webdrivers'
+  gem 'database_cleaner'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -18,9 +18,11 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in "task[name]", with: "new_task_name"
         fill_in "task[content]", with: "new_task_content"
-        fill_in "task[deadline]", with: "1900/01/01"
+        # binding.irb
+        fill_in "task[deadline]", with: "00190001010000"
         click_on("登録する")
-        expect(page).to have_content '1900-01-01 00:00:00 +0900'
+        # binding.irb
+        expect(page).to have_content '1900/01/01 00:00'
       end
     end
   end
@@ -29,13 +31,14 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task, name:'test_task_name_old')
-        task_new = FactoryBot.create(:task, name:'test_task_name_new')
+        # task_old = FactoryBot.create(:task, name:'test_task_name_old')
+        # task_new = FactoryBot.create(:task, name:'test_task_name_new')
+        task_old = FactoryBot.create(:task)
+        task_new = FactoryBot.create(:second_task)
         visit tasks_path
-        
         all('tr td')[7].click_on '詳細'
         
-        expect(page).to have_content 'test_task_name_new'
+        expect(page).to have_content 'test_task_name'
       end
     end
   end
@@ -43,21 +46,63 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面で「終了期限でソートする」をクリックした場合' do
       it '終了期限で降順でソートされたタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old', deadline:'1900/01/01')
-        task_new = FactoryBot.create(:task, name:'test_task_name_deadline_new', deadline:'1900/02/01')
+        # task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old', deadline:'1900/01/01')
+        # task_new = FactoryBot.create(:task, name:'test_task_name_deadline_new', deadline:'1900/02/01')
+        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old')
+        sleep 10
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_deadline_new')
         visit tasks_path
         click_on("終了期限でソートする")
-        # binding.irb
         all('tr td')[7].click_on '詳細'
-        expect(page).to have_content '1900-02-01 00:00:00 +0900'
-        
+        expect(page).to have_content '2010/01/01 18:00'
         visit tasks_path
         click_on("終了期限でソートする")
-        
-        # binding.irb
         sleep 1
         all('tr td')[17].click_on '詳細'
-        expect(page).to have_content '1900-01-01 00:00:00 +0900'
+        expect(page).to have_content '2000/01/01 18:00'
+      end
+    end
+
+    context '一覧画面の検索エリアで「タスク名」を入力して「検索」ボタンを押下した場合場合' do
+      it 'タスク名のあいまい検索で絞り込まれたタスク一覧が表示される' do
+        # task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old', deadline:'1900/01/01')
+        # task_new = FactoryBot.create(:task, name:'test_task_name_deadline_new', deadline:'1900/02/01')
+        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old')
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_search_result')
+        visit tasks_path
+        fill_in "search[name]", with: "search"
+        click_on("検索")
+        all('tr td')[7].click_on '詳細'
+        expect(page).to have_content 'test_task_name_search_result'
+      end
+    end
+
+    context '一覧画面の検索エリアで「ステータス」を入力して「検索」ボタンを押下した場合場合' do
+      it 'ステータスで絞り込まれたタスク一覧が表示される' do
+        # task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old', deadline:'1900/01/01')
+        # task_new = FactoryBot.create(:task, name:'test_task_name_deadline_new', deadline:'1900/02/01')
+        task_old = FactoryBot.create(:task, name:'test_task_name_status_working')
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_status_waiting')
+        visit tasks_path
+        select '着手中',from:'search[status]'
+        click_on("検索")
+        all('tr td')[7].click_on '詳細'
+        expect(page).to have_content 'test_task_name_status_working'
+      end
+    end
+    context '一覧画面の検索エリアで「タスク名」と「ステータス」を入力して「検索」ボタンを押下した場合場合' do
+      it 'タスク名とステータスで絞り込まれたタスク一覧が表示される' do
+        # task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old', deadline:'1900/01/01')
+        # task_new = FactoryBot.create(:task, name:'test_task_name_deadline_new', deadline:'1900/02/01')
+        task_first = FactoryBot.create(:task, name:'test_task_name_status_working')
+        task_second = FactoryBot.create(:second_task, name:'test_task_name_result')
+        task_third = FactoryBot.create(:third_task, name:'test_task_name_status_waiting')
+        visit tasks_path
+        fill_in "search[name]", with: "res"
+        select '未着手',from:'search[status]'
+        click_on("検索")
+        expect(page).to_not have_content 'test_task_name_status_waiting'
+        expect(page).to have_content 'test_task_name_result'
       end
     end
   end
