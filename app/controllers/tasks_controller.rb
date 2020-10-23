de7d9bbf8183ddec6ecc0e35af5b562edfe09255
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-23 10:25:31 +0900

    優先順を高い順にソートする機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,8 @@ class TasksController < ApplicationController
     @tasks = Task.all.order(created_at: "DESC")
     @task_search_params = task_search_params
     @tasks = Task.search(@task_search_params)
-    @tasks = @tasks.order(deadline: "DESC") if params[:sort_expired].present?
+    @tasks = @tasks.order(deadline: "DESC") if params[:sort_expired_deadline].present?
+    @tasks = @tasks.order(priority: "DESC") if params[:sort_expired_priority].present?
     
     # @tasks = @tasks.where('name LIKE ?', "%#{@task_search_params[:name]}%") if @task_search_params[:name].present?
     # @tasks = @tasks.where(status: @task_search_params[:status]) if @task_search_params[:status].present?
