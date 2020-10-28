0c9316de423326404e7f364b817e3574141979ec
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-28 16:50:11 +0900

    自分（ current_user ）以外のユーザのマイページ（userのshow画面）にアクセスしたらタスク一覧に遷移させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,11 @@ class UsersController < ApplicationController
   def show
     if logged_in?
       # binding.irb
-      # if params[:id] == current_user.id
-      #   @user = current_user
-      # else
-      #   redirect_to user_path(current_user.id)
-      # end
-      @user = current_user
+      if params[:id].to_i == current_user.id
+        @user = current_user
+      else
+        redirect_to tasks_path
+      end
     else 
       redirect_to new_session_path
     end
