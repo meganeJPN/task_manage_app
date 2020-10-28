2c7840751090499123bb7db5b15144f38333151e
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-28 09:53:50 +0900

    Userモデルとコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, only:[:index,:new, :create, :edit, :update, :show, :destroy]
+  resources :users, only: [:new, :create]
   # resources :tasks
 end
