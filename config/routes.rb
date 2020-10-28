974ea2cde0d54b9ed10f8afd2795a60f8158cdb3
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-28 15:36:31 +0900

    ログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tasks#index'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks, only:[:index,:new, :create, :edit, :update, :show, :destroy]
   resources :users, only: [:new, :create, :show]
   # resources :tasks
