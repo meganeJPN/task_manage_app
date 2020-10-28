6db26cd395f6fe9a9c707389ad9ed30e73a715bf
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-28 14:47:24 +0900

    ユーザのshowページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id)
     else
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email, :passworrd, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks, only:[:index,:new, :create, :edit, :update, :show, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   # resources :tasks
 end
