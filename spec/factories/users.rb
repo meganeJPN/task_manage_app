cd576bfa084eff2cdbe41c5f08bf3b3505f101fa
meganeJPN <ooosamuuu1985@gmail.com>
2020-10-30 09:46:05 +0900

    不要だったspec/factories/task.rbを削除 admin/user/newで管理者権限の設定ができるように修正 admin/userの権限の表示をtrue/falseから管理者/一般に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'AdminUser' }
+    email { 'admin@sample.jp' }
+    password{'adminadmin'}
+    password_confirmation{'adminadmin'}
+    admin {true}
   end
 end
