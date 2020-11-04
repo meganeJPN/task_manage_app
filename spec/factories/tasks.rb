7357e6852ff896396a4f5e553d32f77c18e03427
meganeJPN <ooosamuuu1985@gmail.com>
2020-11-04 14:07:16 +0900

    TaskのSpecをUserとのアソシエーションを考慮してテストが通るように修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,31 +1,46 @@
 FactoryBot.define do
+  factory :task_user, :class => 'User'  do
+    name { 'taskUser01' }
+    email { 't01@sample.jp' }
+    password{'12345678'}
+    password_confirmation{'12345678'}
+    admin {false}
+  end
   factory :task do
+    # association :user, factory: :common_user
     name { 'test_task_name' }
     content { 'test_task_content' }
     status{'working'}
     priority{'high'}
     deadline {'2000-01-01 18:00:00'}
+    user_id {''}
   end
   
   factory :second_task, class: Task do
+    association :user
     name {'test_task_name_second'}
     content {'test_task_content_second'}
     status{'wating'}
     priority{'low'}
     deadline {'2010-01-01 18:00:00'}
+    user_id{''}
   end
   factory :third_task, class: Task do
+    association :user
     name {'test_task_name_third'}
     content {'test_task_content_second'}
     status{'wating'}
     priority{'low'}
     deadline {'2010-01-01 18:00:00'}
+    user_id{''}
   end
   factory :fourth_task, class: Task do
+    association :user
     name {'test_task_name_third'}
     content {'test_task_content_second'}
     status{'working'}
     priority{'low'}
     deadline {'2010-01-01 18:00:00'}
+    user_id{''}
   end
 end
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -3,7 +3,12 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        visit new_task_path
+        task_user = FactoryBot.create(:task_user)
+        visit new_session_path
+        fill_in "session[email]", with: "t01@sample.jp"
+        fill_in "session[password]", with: "12345678"
+        click_on("Log in")
+        click_on("新規タスクの作成")
         fill_in "task[name]", with: "new_task_name"
         fill_in "task[content]", with: "new_task_content"
         fill_in "task[deadline]", with: "00190001010000"
@@ -14,7 +19,12 @@ RSpec.describe 'タスク管理機能', type: :system do
   
     context 'タスクを新規作成した場合' do
       it '終了期限も登録できる' do
-        visit new_task_path
+        task_user = FactoryBot.create(:task_user)
+        visit new_session_path
+        fill_in "session[email]", with: "t01@sample.jp"
+        fill_in "session[password]", with: "12345678"
+        click_on("Log in")
+        click_on("新規タスクの作成")
         fill_in "task[name]", with: "new_task_name"
         fill_in "task[content]", with: "new_task_content"
         fill_in "task[deadline]", with: "00190001010000"
@@ -25,7 +35,12 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context 'タスクを新規作成した場合' do
       it '優先順位も登録できる' do
-        visit new_task_path
+        task_user = FactoryBot.create(:task_user)
+        visit new_session_path
+        fill_in "session[email]", with: "t01@sample.jp"
+        fill_in "session[password]", with: "12345678"
+        click_on("Log in")
+        click_on("新規タスクの作成")
         fill_in "task[name]", with: "new_task_name"
         fill_in "task[content]", with: "new_task_content"
         select '中',from:'task[priority]'
@@ -39,11 +54,11 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task)
-        task_new = FactoryBot.create(:second_task)
+        task_user = create_and_login(:task_user)
+        task_old = FactoryBot.create(:task,user_id:task_user.id)
+        task_new = FactoryBot.create(:second_task,user_id:task_user.id)
         visit tasks_path
         all('tr td')[7].click_on '詳細'
-        
         expect(page).to have_content 'test_task_name'
       end
     end
@@ -52,8 +67,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面で「終了期限でソートする」をクリックした場合' do
       it '終了期限で降順でソートされたタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old')
-        task_new = FactoryBot.create(:second_task, name:'test_task_name_deadline_new')
+        task_user = create_and_login(:task_user)
+        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old',user_id:task_user.id)
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_deadline_new',user_id:task_user.id)
         visit tasks_path
         click_on("終了期限▼")
         sleep 1
@@ -69,9 +85,10 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context '一覧画面で「優先順位でソートする」をクリックした場合' do
       it '優先順位で降順でソートされたタスク一覧が表示される' do
-        task_middle = FactoryBot.create(:task, name:'test_task_name_priority_middle',priority:'middle')
-        task_low = FactoryBot.create(:second_task, name:'test_task_name_priority_low',priority:'low')
-        task_high = FactoryBot.create(:third_task, name:'test_task_name_priority_high',priority:'high')
+        task_user = create_and_login(:task_user)
+        task_middle = FactoryBot.create(:task, name:'test_task_name_priority_middle',priority:'middle',user_id:task_user.id)
+        task_low = FactoryBot.create(:second_task, name:'test_task_name_priority_low',priority:'low',user_id:task_user.id)
+        task_high = FactoryBot.create(:third_task, name:'test_task_name_priority_high',priority:'high',user_id:task_user.id)
         visit tasks_path
         click_on("優先順位▼")
         sleep 1
@@ -92,8 +109,9 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context '一覧画面の検索エリアで「タスク名」を入力して「検索」ボタンを押下した場合場合' do
       it 'タスク名のあいまい検索で絞り込まれたタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old')
-        task_new = FactoryBot.create(:second_task, name:'test_task_name_search_result')
+        task_user = create_and_login(:task_user)
+        task_old = FactoryBot.create(:task, name:'test_task_name_deadline_old',user_id:task_user.id)
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_search_result',user_id:task_user.id)
         visit tasks_path
         fill_in "search[name]", with: "search"
         click_on("検索")
@@ -104,8 +122,9 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context '一覧画面の検索エリアで「ステータス」を入力して「検索」ボタンを押下した場合場合' do
       it 'ステータスで絞り込まれたタスク一覧が表示される' do
-        task_old = FactoryBot.create(:task, name:'test_task_name_status_working')
-        task_new = FactoryBot.create(:second_task, name:'test_task_name_status_waiting')
+        task_user = create_and_login(:task_user)
+        task_old = FactoryBot.create(:task, name:'test_task_name_status_working',user_id:task_user.id)
+        task_new = FactoryBot.create(:second_task, name:'test_task_name_status_waiting',user_id:task_user.id)
         visit tasks_path
         select '着手中',from:'search[status]'
         click_on("検索")
@@ -115,9 +134,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context '一覧画面の検索エリアで「タスク名」と「ステータス」を入力して「検索」ボタンを押下した場合場合' do
       it 'タスク名とステータスで絞り込まれたタスク一覧が表示される' do
-        task_first = FactoryBot.create(:task, name:'test_task_name_status_working')
-        task_second = FactoryBot.create(:second_task, name:'test_task_name_result')
-        task_third = FactoryBot.create(:third_task, name:'test_task_name_status_waiting')
+        task_user = create_and_login(:task_user)
+        task_first = FactoryBot.create(:task, name:'test_task_name_status_working',user_id:task_user.id)
+        task_second = FactoryBot.create(:second_task, name:'test_task_name_result',user_id:task_user.id)
+        task_third = FactoryBot.create(:third_task, name:'test_task_name_status_waiting',user_id:task_user.id)
         visit tasks_path
         fill_in "search[name]", with: "res"
         select '未着手',from:'search[status]'
@@ -131,6 +151,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+        task_user = create_and_login(:task_user)
         visit new_task_path
         fill_in "task[name]", with: "detail_task_name"
         fill_in "task[content]", with: "detail_task_content"
@@ -140,5 +161,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'detail_task_name'
        end
      end
+  end
+
+  private
+  def create_and_login(user)
+    task_user = FactoryBot.create(user)
+    visit new_session_path
+    fill_in "session[email]", with: "t01@sample.jp"
+    fill_in "session[password]", with: "12345678"
+    click_on("Log in")
+    return task_user
   end
 end
